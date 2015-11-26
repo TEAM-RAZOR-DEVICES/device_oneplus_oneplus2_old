d5efefaeb12f0ad2f2d22485d5effa1b57ff3df2
Gabriel Horn <gabriel_horn@web.de>
2015-11-26 11:13:54 +0100

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,14 +27,6 @@ LOCAL_SRC_FILES		:= etc/init.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := init.qcom.power.rc
-#LOCAL_MODULE_TAGS  := optional eng
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-#include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.sh
 LOCAL_MODULE_TAGS	:= optional eng
@@ -109,11 +101,4 @@ LOCAL_MODULE		:= msm8994_tune.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/msm8994_tune.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= msm8994_aux.sh
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/msm8994_aux.sh
 include $(BUILD_PREBUILT)
